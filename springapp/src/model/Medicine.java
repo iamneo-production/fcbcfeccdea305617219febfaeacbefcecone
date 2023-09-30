cf058c1864cc24dce84660534de304435992860a
Iamneo <techadmin@iamneo.ai>
2023-09-30 09:06:50 +0000

    Backup Commit

---
+++ springapp/src/model/Medicine.java
@@ -0,0 +1,14 @@
+publci class Medicine{
+    private int medicineId;
+    private String medicineName;
+    private float price;
+    private int quantity;
+    private String description;
+
+    public Medicine(int medicineId, String medicineName,float price, int quantity,String description ){
+        this.medicineId = medicineId;
+        this.medicineName = medicineName;
+        this.price = price;
+        
+    }
+}
